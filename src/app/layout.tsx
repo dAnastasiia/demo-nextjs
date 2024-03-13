99a27da34eb053ab15bdd8a8aa9b226a75ea92f2
Anastasiia Drozd <a.drozd@spd-ukraine.com>
2024-03-13 21:25:26 +0200

    add page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import { ReactNode } from 'react';
+
+import type { Metadata } from 'next';
 import { Roboto_Mono } from 'next/font/google';
 
 import './globals.css';
-import { Router } from 'next/router';
 
 const font = Roboto_Mono({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Demo Next.js App',
+};
 
 export default function RootLayout({
   children,
